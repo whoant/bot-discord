aeefbbf3b3b2853f7e2626644c272107532fe530
Vo Van Hoang Tuan <19dh110082@st.huflit.edu.vn>
2021-10-30 12:15:42 +0700

    Update

--- src/googleTranslate.js
+++ src/googleTranslate.js
@@ -1,72 +1,82 @@
 require('dotenv').config();
-const { Client, Intents  } = require('discord.js');
-const { getVoiceConnection, NoSubscriberBehavior, createAudioPlayer, createAudioResource, AudioPlayerStatus  } = require('@discordjs/voice');
+const { Client, Intents } = require('discord.js');
+const {
+    getVoiceConnection,
+    NoSubscriberBehavior,
+    createAudioPlayer,
+    createAudioResource,
+    AudioPlayerStatus,
+} = require('@discordjs/voice');
 const { Player } = require('discord-music-player');
 
 
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
+});
 
-    const client = new Client({
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
-    });
+const TOKEN = process.env.TOKEN_BOT_TRANSLATE;
+const PREFIX = process.env.PREFIX;
 
-    const TOKEN = process.env.TOKEN_BOT_TRANSLATE;
-    const PREFIX = process.env.PREFIX;
+const player = new Player(client, {
+    leaveOnEmpty: false,
+});
 
-    const player = new Player(client, {
-        leaveOnEmpty: false,
-    });
+const queueTranslate = {
+    connection: null,
+    keywords: [],
+};
 
-    const queueTranslate = {
-        connection: null,
-        keywords: []
-    };
+client.player = player;
+client.on('ready', () => {
+    console.log('Online !');
+});
 
-    client.player = player;
-    client.on('ready', () => {
-        console.log('Online !');
-    });
-
-    const nextVoice = () => {
-        if (queueTranslate.keywords.length === 0) {
-            queueTranslate.connection = null;
-            return;
-        }
-
-        const player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause,
-            },
-        });
-
-        queueTranslate.connection.subscribe(player);
-        const text = queueTranslate.keywords[0];
-        const resource = createAudioResource(`http://translate.google.com/translate_tts?tl=vi&q=${text}&client=tw-ob`);
-        player.play(resource);
-        player.on(AudioPlayerStatus.Idle, () => {
-            queueTranslate.keywords.shift();
-            nextVoice(player);
-        });
+const nextVoice = () => {
+    if (queueTranslate.keywords.length === 0) {
+        queueTranslate.connection = null;
+        return;
     }
 
-    client.on('messageCreate', async (message) => {
-        if (!message.content.startsWith(PREFIX)) return;
-
-        const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
-        const command = args.shift();
-
-        if (command === 'g') {
-            const keyword = args.join(' ');
-            queueTranslate.keywords.push(keyword);
-            console.log(queueTranslate.keywords);
-            if (queueTranslate.connection === null) {
-
-                const queue = client.player.createQueue(message.guild.id);
-                await queue.join(message.member.voice.channel);
-                queueTranslate.connection = getVoiceConnection(message.guild.id);
-                nextVoice();
-            }
-        }
+    const player = createAudioPlayer({
+        behaviors: {
+            noSubscriber: NoSubscriberBehavior.Pause,
+        },
     });
 
-    client.login(TOKEN);
+    queueTranslate.connection.subscribe(player);
+    const text = queueTranslate.keywords[0];
+    const resource = createAudioResource(`http://translate.google.com/translate_tts?tl=vi&q=${text}&client=tw-ob`);
+    player.play(resource);
+    player.on(AudioPlayerStatus.Idle, () => {
+        queueTranslate.keywords.shift();
+        nextVoice(player);
+    });
+};
+
+client.on('messageCreate', async (message) => {
+    const text = message.content.toLowerCase();
+    if (text.includes("tuan") || text.includes('tuân')){
+        message.reply("a T cute đúng ko :3");
+    }
+
+    if (!message.content.startsWith(PREFIX)) return;
+
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+    const command = args.shift();
+
+    if (command === 'g') {
+        const keyword = args.join(' ');
+        queueTranslate.keywords.push(keyword);
+        // console.log(queueTranslate.keywords);
+        if (queueTranslate.connection === null) {
+
+            const queue = client.player.createQueue(message.guild.id);
+            await queue.join(message.member.voice.channel);
+            queueTranslate.connection = getVoiceConnection(message.guild.id);
+            nextVoice();
+        }
+    }
+});
+
+client.login(TOKEN);
 
