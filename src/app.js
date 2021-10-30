118c5b9f3a7c3b62abe3604b15f41de84fba9a9a
Vo Van Hoang Tuan <19dh110082@st.huflit.edu.vn>
2021-10-30 11:41:37 +0700

    Update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { Client, Intents  } = require('discord.js');
-const { getVoiceConnection, NoSubscriberBehavior, createAudioPlayer, createAudioResource, AudioPlayerStatus  } = require('@discordjs/voice');
 const { Player, RepeatMode } = require('discord-music-player');
 
 const { regExpPlaylist } = require('./regExp');
@@ -18,10 +17,7 @@ const player = new Player(client, {
     leaveOnEmpty: false,
 });
 
-const queueTranslate = {
-    connection: null,
-    keywords: []
-};
+
 
 client.player = player;
 
@@ -31,48 +27,12 @@ module.exports = () => {
         console.log('Online !');
     });
 
-    const nextVoice = () => {
-        if (queueTranslate.keywords.length === 0) {
-            queueTranslate.connection = null;
-            return;
-        }
-
-        const player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause,
-            },
-        });
-
-        queueTranslate.connection.subscribe(player);
-        const text = queueTranslate.keywords[0];
-        const resource = createAudioResource(`http://translate.google.com/translate_tts?tl=vi&q=${text}&client=tw-ob`);
-        player.play(resource);
-        player.on(AudioPlayerStatus.Idle, () => {
-            queueTranslate.keywords.shift();
-            nextVoice(player);
-        });
-    }
 
     client.on('messageCreate', async (message) => {
         if (!message.content.startsWith(PREFIX)) return;
 
         const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
         let guildQueue = client.player.getQueue(message.guild.id);
-
-        const command = args.shift();
-
-        if (command === 'g') {
-            const keyword = args.join(' ');
-            queueTranslate.keywords.push(keyword);
-            console.log(queueTranslate.keywords);
-            if (queueTranslate.connection === null) {
-
-                const queue = client.player.createQueue(message.guild.id);
-                await queue.join(message.member.voice.channel);
-                queueTranslate.connection = getVoiceConnection(message.guild.id);
-                nextVoice();
-            }
-        }
 
         if (command === 'p') {
             const keyword = args.join(' ');
