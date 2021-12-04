b4a57736b4c55fd2118e390799b38811b588cbb7
Vo Van Hoang Tuan <vovanhoangtuan4.2@gmail.com>
2021-12-04 21:02:10 +0700

    Add voice Zalo

---
+++ src/ZaloVoice.js
@@ -0,0 +1,53 @@
+const request = require('request-promise');
+
+const getVoiceURL = async text => {
+    const options = {
+        method: 'POST',
+        uri: 'https://zalo.ai/api/demo/v1/tts/synthesize',
+        headers: {
+            'origin': 'https://zalo.ai',
+            'referer': 'https://zalo.ai/experiments/text-to-audio-converter',
+            'cookie': 'zai_did=8k9uAj3FNiTevcSSryzXoYYo64l2pM_0BhSGG3Wq; _ga=GA1.2.1760285536.1636389062; zpsid=eMKnVbo-PZEpJXydFyOGSQr45YGzpmPrhLjXN7w0RKkaJMjGDBraRDT-2MCGdbumsHqh74BXPH6bOnCRQf5wD8rOBJLNaIKcfdzDQ7EmC5EEVJP8C0; zai_sid=j8E_PzawGLUgYRPaspDNKwgMmHoOV3njeu3B4D91DKhGiwaZzc8F5jxGYroa7JGtegwpMunjAnAAbOrriMrq0A6oxIYLJ65rYR2M2FeO8spGp9y0P0; _zlang=vn; __zi=3000.SSZzejyD0jydXQcYsa00d3xBfxgP71AM8Tdbg8yFMSbdsktWmmLGocNJgUV71n_DOzchvy896iy.1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+        },
+        form: {
+            input: text,
+            speaker_id: 1,
+            speed: 1,
+            dict_id: 0,
+        },
+        json: true,
+    };
+    try {
+        const { error_code, data, error_message } = await request(options);
+        if (error_code === 0) return Promise.resolve(data.url);
+        return Promise.reject(error_message);
+    } catch (e) {
+        return Promise.reject(e);
+    }
+};
+
+const getLinkMp3 = async uri => {
+    try {
+        const res = await request({
+            uri,
+        });
+        const splitData = res.split('\n');
+        const newLink = `https:${splitData[8]}`;
+        return Promise.resolve(newLink);
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+const createVoiceZalo = async text => {
+    try {
+        const m3u8Link = await getVoiceURL(text);
+        const newLink = await getLinkMp3(m3u8Link);
+        return Promise.resolve(newLink);
+    } catch (e) {
+        return Promise.reject(e);
+    }
+};
+
+module.exports = createVoiceZalo;
--- src/googleTranslate.js
+++ src/googleTranslate.js
@@ -9,11 +9,13 @@ const {
 } = require('@discordjs/voice');
 const { Player } = require('discord-music-player');
 
-const low = require('lowdb')
+const low = require('lowdb');
 
-const FileSync = require('lowdb/adapters/FileSync')
+const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('db.json')
+const ZaloVoice = require('./ZaloVoice');
+
+const adapter = new FileSync('db.json');
 const db = low(adapter);
 db.defaults({ filters: [] })
     .write();
@@ -29,6 +31,9 @@ const player = new Player(client, {
     leaveOnEmpty: false,
 });
 
+
+let SWITCH_VOICE = 'google';
+
 const queueTranslate = {
     connection: null,
     keywords: [],
@@ -40,7 +45,7 @@ client.on('ready', () => {
     console.log('Online !');
 });
 
-const nextVoice = () => {
+const nextVoice = async () => {
     if (queueTranslate.keywords.length === 0) {
         queueTranslate.connection = null;
         return;
@@ -52,14 +57,28 @@ const nextVoice = () => {
         },
     });
 
-    queueTranslate.connection.subscribe(player);
-    const text = queueTranslate.keywords[0];
-    const resource = createAudioResource(`http://translate.google.com/translate_tts?tl=vi&q=${text}&client=tw-ob`);
-    player.play(resource);
-    player.on(AudioPlayerStatus.Idle, () => {
-        queueTranslate.keywords.shift();
-        nextVoice(player);
-    });
+    try {
+        queueTranslate.connection.subscribe(player);
+        const text = queueTranslate.keywords[0];
+        let resource;
+        if (SWITCH_VOICE === 'zalo') {
+            const linkZalo = await ZaloVoice(text);
+            resource = createAudioResource(linkZalo);
+        } else {
+            resource = createAudioResource(`http://translate.google.com/translate_tts?tl=vi&q=${text}&client=tw-ob`);
+        }
+
+        player.play(resource);
+        player.on(AudioPlayerStatus.Idle, () => {
+            queueTranslate.keywords.shift();
+            nextVoice(player);
+        });
+
+    } catch (e) {
+        console.log('=== ERROR NEXT_VOICE ===');
+        console.error(e);
+    }
+
 };
 
 client.on('messageCreate', async (message) => {
@@ -67,7 +86,7 @@ client.on('messageCreate', async (message) => {
 
     if (message.author.bot) return;
     const checkFilter = checkKeyWords(text);
-    if (checkFilter !== ''){
+    if (checkFilter !== '') {
         message.reply(checkFilter);
     }
 
@@ -90,18 +109,24 @@ client.on('messageCreate', async (message) => {
 
     if (command === 'leave') {
         const guidId = message.guild.id;
-        message.reply("Dỗi á nha :( ");
+        message.reply('Dỗi á nha :( ');
         queueTranslate.connection = null;
         getVoiceConnection(guidId).disconnect();
     }
 
-    if (command === 'filter') {
+    if (command === 'voice') {
+        if (message.author.id !== '600202809115410451') return;
+        const keyword = args.join(' ');
+        SWITCH_VOICE = keyword;
+        message.reply(`Chuyển voice: ${keyword} thành công !`);
+    }
 
+    if (command === 'filter') {
         if (message.author.id !== '600202809115410451') return;
         const keyword = args.join(' ');
         const [key, word] = keyword.split('>');
-        db.get('filters').push({key: key.trim(), word: word.trim() }).write();
-        message.reply("Thêm thành công :3");
+        db.get('filters').push({ key: key.trim(), word: word.trim() }).write();
+        message.reply('Thêm thành công :3');
 
     }
 
@@ -110,7 +135,6 @@ client.on('messageCreate', async (message) => {
 
 function checkKeyWords(text) {
     const listKey = db.get('filters').value();
-
     const filter = listKey.find(item => text.includes(item.key));
     if (filter === undefined) return '';
     return filter.word;
